3df37336778b07284a907dc3ffc7f16e7a031ee1
Sandeep Kumar <sandeepkumar75255@gmail.com>
2025-12-01 15:07:59 +0530

    Configure GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/URO-Flood-Lab-Web-Platform/",
   plugins: [
     tailwindcss(),
     react()
