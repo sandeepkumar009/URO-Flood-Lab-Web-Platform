7f8e943c27ac1a27d8ddff736226cc56e712e23a
Sandeep Kumar <sandeepkumar75255@gmail.com>
2025-12-02 18:03:23 +0530

    revert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/URO-Flood-Lab-Web-Platform/",
   plugins: [
     tailwindcss(),
     react()
